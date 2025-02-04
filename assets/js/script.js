e64ba8877c6ce1f7b7ef810e7a4bf9d09e9f7d84
amine <amineemails@gmail.com>
2025-02-04 21:15:27 +0100

    image slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
     $("#CheckIn").datepicker();
     $("#CheckOut").datepicker();
+
+    $('.level-slider').slick({
+        dots: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+    });
 });
